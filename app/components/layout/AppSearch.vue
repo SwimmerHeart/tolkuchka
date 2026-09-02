11ca375048c1029fdf15aa035f9297fd892b6928
SwimmerHeart <serg.decline@gmail.com>
2026-09-02 10:17:05 +0300

    feat: шапка - меню категорий и поиск (AppSearch, мобильная строка)

---
+++ app/components/layout/AppSearch.vue
@@ -0,0 +1,19 @@
+<template>
+  <UInput
+    :model-value="q"
+    icon="i-heroicons-magnifying-glass"
+    placeholder="Поиск по каталогу"
+    class="min-w-0 flex-1"
+    @update:model-value="store.setQuery($event)"
+    @keydown.enter="navigateTo('/products')"
+  />
+</template>
+
+<script setup lang="ts">
+  const store = useProductStore();
+
+  const q = computed({
+    get: () => store.filters.q,
+    set: (v: string) => store.setQuery(v),
+  });
+</script>
